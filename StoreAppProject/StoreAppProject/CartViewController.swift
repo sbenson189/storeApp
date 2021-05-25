99f78fb192eb9ec111822d02e8da837b76a716c3
Yazid Louda <72033741+yazidlouda@users.noreply.github.com>
2021-05-25 17:16:44 -0400

    Revert "Added item list item details and add to cart"

--- StoreAppProject/StoreAppProject/CartViewController.swift
+++ StoreAppProject/StoreAppProject/CartViewController.swift
@@ -8,46 +8,22 @@
 import UIKit
 
 class CartViewController: UIViewController {
-    var cartInstance = Cart.sharedInstance
-    @IBOutlet weak var total: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let nib = UINib(nibName: "CartTableViewCell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "CartTableViewCell")
-        tableView.delegate = self
-        tableView.dataSource = self
-        print(cartInstance.cartItems as Any)
-        tableView.rowHeight = 150
+
         // Do any additional setup after loading the view.
     }
     
 
-    override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
-           tableView.reloadData()
-        total.text = "$" + cartInstance.getTotal().description
-    }
+    /*
+    // MARK: - Navigation
 
-}
-extension CartViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cartInstance.cartItems.count
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell  = tableView.dequeueReusableCell(withIdentifier: "CartTableViewCell", for: indexPath) as! CartTableViewCell
-        cell.itemImage.image = cartInstance.cartItems[indexPath.row].image
-        cell.itemName.text = cartInstance.cartItems[indexPath.row].name
-        cell.itemDescription.text = cartInstance.cartItems[indexPath.row].description
-        cell.itemPrice.text = "$" + cartInstance.cartItems[indexPath.row].price.description
-        return cell
-    }
-    
-    
-}
+    */
 
-extension CartViewController: UITableViewDelegate {
-    
 }
--- StoreAppProject/StoreAppProject/UserDashboardViewController.swift
+++ StoreAppProject/StoreAppProject/UserDashboardViewController.swift
@@ -6,74 +6,24 @@
 //
 
 import UIKit
-import UserNotifications
-
 
 class UserDashboardViewController: UIViewController {
-    @IBOutlet weak var categoryCollectionView: UICollectionView!
-    
-    @IBOutlet weak var dealsCollectionView: UICollectionView!
-    @IBOutlet weak var popularCollectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    var catogeries:[Category] = [
-        .init(id: "id1", name: "Egypt Dishs", image: "https://picsum.photos/100/200"),
-        .init(id: "id2", name: "Oman Dishs", image: "https://picsum.photos/100/200"),
-        .init(id: "id3", name: "jordn Dishs", image: "https://picsum.photos/100/200"),
-        .init(id: "id4", name: "USA Dishs", image: "https://picsum.photos/100/200"),
-        .init(id: "id5", name: "Qanter Dishs", image: "https://picsum.photos/100/200")
-    ]
-    
-    var popualr:[Product1] = [
-        .init(id: "id1", image: "https://picsum.photos/100/200", description: "Nice And Beuti Pizza Nice And Beuti Pizza Nice And Beuti Pizza"),
-        .init(id: "id2", image: "https://picsum.photos/100/200",description: "Nice And Beuti Indomi Nice And Beuti Indomi Nice And Beuti Indomi"),
-        .init(id: "id3", image: "https://picsum.photos/100/200", description: "Nice And Beuti Cola Nice And Beuti Cola Nice And Beuti Cola"),
-        .init(id: "id4", image: "https://picsum.photos/100/200",description: "Nice And Beuti Kofta Nice And Beuti Kofta Nice And Beuti Kofta"),
-        .init(id: "id5", image: "https://picsum.photos/100/200", description: "Nice And Beuti Bashamel Nice And Beuti Bashamel"),
-    ]
-    
-    var deals:[Product1] = [
-        
-        .init(id: "id1", image: "https://picsum.photos/100/200", description: "Nice And Beuti Batata Nice And Beuti Batata"),
-        .init(id: "id2", image: "https://picsum.photos/100/200", description: "Nice And Beuti Pepsi Nice And Beuti Pepsi Nice And Beuti Pepsi"),
-        .init(id: "id3", image: "https://picsum.photos/100/200",  description: "Nice And Beuti Mahshey Nice And Beuti Mahshey"),
-        .init(id: "id4", image: "https://picsum.photos/100/200", description: "Nice And Beuti nodels Nice And Beuti nodels Nice And Beuti nodels"),
-    ]
-   
-  
-    
-    //MARK: -> Class Methods
-    
-  
-    
-    func addLocalNotifaction() {
-        
-        //Ask Permission
-        let centre = UNUserNotificationCenter.current()
-        centre.requestAuthorization(options: [.alert , .sound]) { (grant, error) in
-            
-            if error == nil {
-                print("Error: \(error?.localizedDescription ?? "")")
-            }
-        }
-        //Create Notifaction Content
-        let content = UNMutableNotificationContent()
-        content.title = "HI 🔴"
-        content.body = "Come Again To See New Items.🚚🚛"
-        content.sound = .default
-        
-        //Create Notifaction Trigger
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 7, repeats: false)
-        //Create Request
-        let uuidString = UUID().uuidString
-        let reqeust  = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
-        centre.add(reqeust) { (error) in
-            
-        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
